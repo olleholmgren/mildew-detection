153cf9be14de6faf58cf4f65ded88b03e4b62d25
olleholmgren <>
2023-10-04 11:47:02 +0000

    adjusted lines in hypothesis.py and summary.py files

--- app_pages/summary.py
+++ app_pages/summary.py
@@ -6,5 +6,7 @@ def summary_body():
 
     st.success(
         F'This project has 2 business requirements:\n'
-        F'* The client is interested in conducting a study to visually differentiate a healthy cherry leaf from one with powdery mildew.\n\n'
-        F'* The client is interested in predicting if a cherry leaf is healthy or contains powdery mildew.')
+        F'* The client is interested in conducting a study to visually 
+        differentiate a healthy cherry leaf from one with powdery mildew.\n\n'
+        F'* The client is interested in predicting if a cherry leaf is 
+        healthy or contains powdery mildew.')
