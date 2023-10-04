f5b8b82c601b7774d12ac87bdf722ec7e80c3624
olleholmgren <>
2023-10-04 13:45:39 +0000

    create path for visulizer page in streamlit app

--- app.py
+++ app.py
@@ -3,10 +3,12 @@ from app_pages.multi_page import MultiPage
 
 from app_pages.hypothesis import hypothesis_body
 from app_pages.summary import summary_body
+from app_pages.visualizer import visualizer_body
 
 app = MultiPage(app_name='Cherry leaf mildew detection')
 
 app.app_page('Hypothesis', hypothesis_body)
 app.app_page('Summary', summary_body)
+app.app_page('Visualizer', visualizer_body)
 
 app.run()
