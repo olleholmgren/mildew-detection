8029ffb05484e7d19d19fc0db7082a2f50876466
olleholmgren <>
2023-10-05 12:30:17 +0000

    create src folder with data_management file. create mildew_detection page for streamlit dashboard app

--- app.py
+++ app.py
@@ -4,11 +4,13 @@ from app_pages.multi_page import MultiPage
 from app_pages.hypothesis import hypothesis_body
 from app_pages.summary import summary_body
 from app_pages.visualizer import visualizer_body
+from app_pages.mildew_detection import mildew_detection_body
 
 app = MultiPage(app_name='Cherry leaf mildew detection')
 
 app.app_page('Hypothesis', hypothesis_body)
 app.app_page('Summary', summary_body)
 app.app_page('Visualizer', visualizer_body)
+app.app_page('Mildew Detection', mildew_detection_body)
 
 app.run()
