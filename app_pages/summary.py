ea0aaf249139b08d5122bc495eeee29661ec2088
olleholmgren <>
2023-10-12 12:23:00 +0000

    corrected an f-string in summary.py

--- app_pages/summary.py
+++ app_pages/summary.py
@@ -7,7 +7,7 @@ def summary_body():
     st.success(
         f'This project has 2 business requirements:\n'
         f'* The client is interested in conducting a study to visually '
-        f'differentiate a healthy cherry leaf'
+        f'differentiate a healthy cherry leaf '
         f'from one with powdery mildew.\n\n'
         f'* The client is interested in predicting if a cherry leaf is '
         f'healthy or contains powdery mildew.')
